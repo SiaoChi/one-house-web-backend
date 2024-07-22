2473640a169f25f37ec3dd1679acedbbae0a2a55
siaochi <chichigou.work@gmail.com>
2024-07-22 11:22:37 +0800

    Add: Home page comment for create new projet

--- one-house/src/components/Home.js
+++ one-house/src/components/Home.js
@@ -1,25 +1,33 @@
-// src/components/Home.js
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import '../css/Home.css'
 
 const Home = () => {
-  const containerStyle = {
-    textAlign: 'center' // 將容器內所有內容置中
-  };
-
-  const headingStyle = {
-    marginBottom: '20px' // 標題下方留出一些間距
-  };
-
   return (
-    <div style={containerStyle}>
-      <h1 style={headingStyle}>One-House 官網後台</h1>
-      <p>Github Repo：One-house-web</p>
-      <p>fronted：next.js</p>
-      <p>Backend：API AWS lambda</p>
-      <p>database：Firestore</p>
-      <p>dashboard：Firestore + react + s3 + cloudfront</p>
-    </div>
+    <>
+      <div className="container">
+        <div className="section">
+          <h3>建立專案說明</h3>
+          <ol>
+            <li>手動將檔案丟到指定的s3資料夾。前端網頁會直接加入s3/CDN的domain</li>
+            <li>建立檔案的圖片，請直接輸入檔案名稱</li>
+            <li>由於專案desc by timestamp，建立時請以最舊的資料建立到最新資料。未來可以在開發手動建立日期對應timestamp較方便。</li>
+          </ol>
+        </div>
+      </div>
+      <p></p>
+      <div className="container">
+        <div className="section">
+          <h3>使用語言與技術</h3>
+          <ol>
+            <li>Github Relio：One-house-web</li>
+            <li>fronted：next.js</li>
+            <li>Backend：AliI AWS lambda</li>
+            <li>database：Firestore</li>
+            <li>dashboard：Firestore + react + s3 + cloudfront + gh-liages</li>
+          </ol>
+        </div>
+      </div>
+    </>
   );
 };
 
